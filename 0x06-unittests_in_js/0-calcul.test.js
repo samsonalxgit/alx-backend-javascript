47bdf2011974be1d9793b652412bddceb517a3b8
Samson <samsonmanie2022@gmail.com>
2024-09-20 19:04:24 +0000

    update

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,26 +1,11 @@
-const calculateNumber = require('./0-calcul.js');
 const assert = require('assert');
+const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function () {
-  for (let i = 0; i < 26; i++) {
-    it('should return 4', function () {
-      assert.equal(calculateNumber(1, 3), 4);
-    });
-
-    it('should return 5', function () {
-      assert.equal(calculateNumber(1, 3.7), 5);
-    });
-
-    it('should return 5', function () {
-      assert.equal(calculateNumber(1.2, 3.7), 5);
-    });
-
-    it('should return 6', function () {
-      assert.equal(calculateNumber(1.5, 3.7), 6);
-    });
-
-    it('should return 7', function () {
-      assert.equal(calculateNumber(1.6, 5.2), 7);
-    });
-  }
+describe('calculateNumber', function() {
+  it('should return the sum of rounded numbers', function() {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
 });
